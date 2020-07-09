c0f83415257ba3bf4ef91cd5982da2b6fd602c5f
Capitol Heel <66080081+epachols@users.noreply.github.com>
2020-07-08 17:36:20 -0700

    finishing touches, styling

--- Develop/app.js
+++ Develop/app.js
@@ -16,6 +16,12 @@ const teamArr = [];
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
+// this answer validator handles the user trying to enter an empty string 
+const AnswerValidator = async (input) => {
+    if (input === '') {
+       console.log('The Devil is in the Details. Please provide a string answer.');
+    } else return true;
+ };
 
 function buildTeam() {
     inquirer.prompt({
@@ -36,21 +42,25 @@ function buildTeam() {
                 break;
 
             case "Add the Manager":
-                console.log('Adding the Manager')
-                createManager();
+                // if(teamArr.filter(member => member.role === "Manager").length>0) {
+                // console.log("ManageLander: There Can Be Only One")
+                // buildTeam();
+                // } 
+                let [yesManager] = teamArr.filter(emp => emp.officeNumber)
+                if (yesManager) {
+                    console.log("E Says we Already Got one! ManageLander: There can be only One");
+                    buildTeam();
+                }
+                else {
+                    console.log('Adding the Manager')
+                    createManager();
+                }
                 break;
 
             case "Generate Our TeamPage":
-                console.log("Now making your Team Page, \n Thank you For using CodeCrow's Team Template Engine")
                 //WORKSPACE**
-                
-                
-                if (teamArr) {
-
-                    // The below code block throws a minor error - no such file or directory, error no -4058. maybe  have to first MAKE a file called team in outputPath, and THEN call fs.writeFile. or maybe we need fs.writeToFile? Upon trying some dated fixes with googlefu, still no functional answer. ask tomorrow.
-                    //  maybe declare the below function globally, wrap the below in an `if (outputPath) {} else {make a filepath and then write}`
-                    //***update:made the folder manually already, still not pathing correctly.***
-                                   
+                if (teamArr.length >0) {
+                    console.log("Now making your Team Page, \n Thank you For using CodeCrow's Team Template Engine")
                     fs.writeFile(outputPath, render(teamArr), function(err) {
                         if (err) {
                                 return console.log(err);
@@ -58,10 +68,9 @@ function buildTeam() {
                             console.log("Successfully Written TeamPage, check your output folder");
                     })
 
-                } else {console.log('We need a team to build a team!')}
-                                    
-                //WORKSPACE**  
-
+                } else {
+                    console.log('We need a team to build a team!')
+                }                  
                 break;
                 
             default:
@@ -70,27 +79,31 @@ function buildTeam() {
         })
     }
  
-function createEngineer() {
-    inquirer.prompt([
+async function createEngineer() {
+    await inquirer.prompt([
         {
             type: "input",
             message: "Employee's Name?",
-            name: "name"
+            name: "name",
+            validate: AnswerValidator
         },
         {
             type: "input",
             message: "Employee ID?",
-            name: "id"
+            name: "id",
+            validate: AnswerValidator
         },
         {
             type: "input",
             message: "Employee Email?",
-            name: "email"
+            name: "email",
+            validate: AnswerValidator
         },
         {
             type: "input",
             message: "Employee github name?",
-            name: "github"
+            name: "github",
+            validate: AnswerValidator
         },
     ]).then(function(answers){
         const newEngineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
@@ -102,28 +115,31 @@ function createEngineer() {
     )
 }
 
-
-function createIntern() {
-    inquirer.prompt([
+async function createIntern() {
+    await inquirer.prompt([
         {
          type: "input",
          message: "Employee's Name?",
-         name: "name"
+         name: "name",
+         validate: AnswerValidator
      },
      {
          type: "input",
          message: "Employee ID?",
-         name: "id"
+         name: "id",
+         validate: AnswerValidator
      },
      {
          type: "input",
          message: "Employee Email?",
-         name: "email"
+         name: "email",
+         validate: AnswerValidator
      },
      {
          type: "input",
          message: "School Attending?",
-         name: "school"
+         name: "school",
+         validate: AnswerValidator
      },
  ]).then(function(answers){
     const newIntern = new Intern(answers.name, answers.id, answers.email, answers.school);
@@ -134,37 +150,32 @@ function createIntern() {
 )
 }
 
-function createManager() {
-// Two possible options for solution to issue of trying to input more than one manager:
+async function createManager() {
 
-//if (!teamArr.includes(Manager) {add one} else {buildTeam()})
-//
-// if(teamArr.filter(member => member.role === "Manager").length>0) {
-// console.log("E Sayz We Already Got One!")
-// buildTeam();
-// } else {sandwich inquirer info in here}
-//
-
- inquirer.prompt([
+ await inquirer.prompt([
      {
          type: "input",
          message: "Manager's Name?",
-         name: "name"
+         name: "name",
+         validate: AnswerValidator
      },
      {
          type: "input",
          message: "Manager ID?",
-         name: "id"
+         name: "id",
+         validate: AnswerValidator
      },
      {
          type: "input",
          message: "Manager's Email?",
-         name: "email"
+         name: "email",
+         validate: AnswerValidator
      },
      {
          type: "input",
          message: "Office Number?",
-         name: "officeNumber"
+         name: "officeNumber",
+         validate: AnswerValidator
      },
  ]).then(function(answers){
     const newManager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
@@ -176,6 +187,8 @@ function createManager() {
 }
 
 buildTeam();
+
+
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
